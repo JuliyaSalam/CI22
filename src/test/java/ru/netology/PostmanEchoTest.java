cb29fff1817f1a307a0048138c3a8c1b19792774
Jul <yuosalamatova@gmail.com>
2022-06-13 22:34:23 +0700

    bug UTFg

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -33,7 +33,7 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .header("Content-Type", "application/json; charset=UTF-8")
+                .header("Content-Type", "application/json; charset=UTFg-8")
                 .body("data", equalTo("some data"))
         ;
     }
